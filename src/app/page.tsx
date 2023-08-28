d65546d95a672ec2fec8486c9e6e83e17bcba2a9
JereUR <jeremias.jdv@gmail.com>
2023-08-28 20:31:43 -0300

    feat(Root): Connection to supabase and get posts from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select('*')
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hola Twitter 👋
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </main>
   )
 }

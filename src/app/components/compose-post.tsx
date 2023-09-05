f9ef73a3151151e72b6e54f679e7b687f76df7ba
JereUR <jeremias.jdv@gmail.com>
2023-09-04 22:24:48 -0300

    feat(Root): Use server action and useRef to clear form on send tweet

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -1,24 +1,33 @@
-import { cookies } from 'next/headers'
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
-import { Avatar } from '@nextui-org/react'
+'use client'
+
+import { ComposePostButton } from './compose-post-button'
+import { addPost } from '../actions/add-post-action'
+import { useRef } from 'react'
 
 export function ComposePost({ userAvatarUrl }: { userAvatarUrl: string }) {
+  const formRef = useRef<HTMLFormElement>(null)
+
   return (
-    <form className="flex flex-row space-x-4 p-4 border-b border-white/20">
+    <form
+      ref={formRef}
+      action={async (formData) => {
+        await addPost(formData)
+        formRef.current?.reset()
+      }}
+      className="flex flex-row p-3 border-b border-white/20"
+    >
       <img
-        className="rounded-full w-10 h-10 object-contain"
+        className="rounded-full w-10 h-10 object-contain mr-4"
         src={userAvatarUrl}
       />
       <div className="flex flex-1 flex-col gap-y-4">
         <textarea
-          name="post"
+          name="content"
           rows={4}
           className="w-full text-xl bg-black placeholder-gray-500 p-2"
           placeholder="¡¿Qué está pasando?!"
         ></textarea>
-        <button className="bg-sky-500 font-bold rounded-full px-5 py-2 self-end">
-          Postear
-        </button>
+        <ComposePostButton />
       </div>
     </form>
   )
